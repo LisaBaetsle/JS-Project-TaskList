6ceb811467cc9e5dd5962a8eb724d02c8da20fe9
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-11-09 16:17:06 +0100

    add functionality to remove, clear and filter the tasks

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 };
 
 //Add Task
@@ -42,4 +48,35 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+};
+
+// Remove task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure you want to remove this task?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  };
+};
+
+// Clear all tasks
+function clearTasks() {
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+};
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent; // this takes the text of each element
+    //The indexOf() method returns the position of the first occurrence of a specified value in a string. This method returns -1 if the value to search for never occurs.
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
